370f658f1e8866eb1647ad6747f0683dd8458bf8
lrolvr <larissa.aro@puccampinas.edu.br>
2024-08-23 23:56:48 -0300

    Tarefa 002 - Exercício 2 - Linguagem C

--- EXERCICIOS/TAREFA 002/EXERCICIO 2.cpp
+++ EXERCICIOS/TAREFA 002/EXERCICIO 2.cpp
@@ -1,3 +1,8 @@
+/* Exercicio 2: Imagine que você está desenvolvendo um aplicativo de gestão financeira pessoal. 
+O objetivo do aplicativo é ajudar os usuários a registrar suas receitas e despesas diárias e calcular a soma 
+dos valores para obter um saldo atualizado. Um dos recursos do aplicativo é permitir que o usuário insira dois valores 
+(uma receita e uma despesa) e exiba a soma desses valores para atualizar o saldo.*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
